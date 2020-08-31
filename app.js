5035b6e910e1c7ced645292a6b582b66a7bc544e
beata <beata@emc2wave.com>
2020-08-30 23:05:59 -0700

    add e.currentTarget.getBoundingClientRect

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ sidebar.innerHTML = sublinks
 
 linkBtns.forEach((btn) => {
   btn.addEventListener('mouseover', (e) => {
-    console.log(e.currentTarget);
+    const text = e.currentTarget.textContent;
+    const tempBtn = e.currentTarget.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    submenu.classList.add('show');
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
   });
 });
