9690d7ba4f6f60ba24e8362da3f8d916918d6ff7
beata <beata@emc2wave.com>
2020-08-31 14:29:41 -0700

    add columns = col-2

--- app.js
+++ app.js
@@ -50,10 +50,14 @@ linkBtns.forEach((btn) => {
       submenu.classList.add('show');
       submenu.style.left = `${center}px`;
       submenu.style.top = `${bottom}px`;
+
+      // Optional
+      let columns = 'col-2';
+
       submenu.innerHTML = `
       <section>
         <h4>${page}</h4>
-        <div class="submenu-center col-2">
+        <div class="submenu-center ${columns}">
           ${links
             .map((link) => {
               return `
